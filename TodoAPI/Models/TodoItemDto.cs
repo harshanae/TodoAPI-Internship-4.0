be14450858da04b539e22c98dd954448545c9bd9
husseyhh <56592096+husseyhh@users.noreply.github.com>
2022-01-04 08:28:36 +0530

    Add project files.

---
+++ TodoAPI/Models/TodoItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAPI.Models
+{
+    public class TodoItemDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
+
